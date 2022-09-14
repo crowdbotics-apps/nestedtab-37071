2323a5f68e6d8538c885b740e80e9bfc82cf83d7
Crowdbotics <team@crowdbotics.com>
2022-09-14 11:32:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "nestedtab_37071",
+  url: "https://nestedtab_37071.botics.co",
+  api: "https://nestedtab_37071.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
